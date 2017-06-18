a6dab96f39d30303f39078f5644aaf5fe6137b11
CuityMiko <15201776956@163.com>
2017-06-19 03:21:27 +0800

    修改movieController

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -7,12 +7,12 @@ var movieServices = mypro.services.movie;
 // 获取电影列表
 var getlist=function(req, res, next){
     var obj= {
-        start: parseInt(req.query.pageindex-1)*parseInt(req.query.pagecount),
-        count:parseInt(req.query.pagecount),
+        start: parseInt(req.query.start),
+        count:parseInt(req.query.count),
         city:req.query.city,
         q:req.query.q
     }
-    var promise=movieServices.getmovielist(req.query.classify,obj);
+    var promise=movieServices.getmovielist(req.params.movietype,obj);
     promise.then((data)=>{
         res.json({result:true,data:data});
     },(err)=>{
@@ -23,12 +23,12 @@ var getlist=function(req, res, next){
 // post方式获取电影列表
 var postlist=function(req, res, next){
     var obj= {
-        start: parseInt(req.body.pageindex-1)*parseInt(req.body.pagecount),
-        count:parseInt(req.body.pagecount),
+        start: parseInt(req.body.start),
+        count:parseInt(req.body.count),
         city:req.body.city,
         q:req.body.q
     }
-    var promise=movieServices.getmovielist(req.body.classify,obj);
+    var promise=movieServices.getmovielist(req.params.movietype,obj);
     promise.then((data)=>{
         res.json({result:true,data:data});
     },(err)=>{
